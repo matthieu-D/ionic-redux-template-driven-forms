657c7fe2703acf0530be8c28bcdead96e58a7cd7
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-09-16 16:55:27 -0400

    all working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { defaultFormReducer} from '@angular-redux/form';
+import { NgRedux, DevToolsExtension } from '@angular-redux/store';
+
+declare var require;
+
+var reduxLogger = require('redux-logger');
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public store: NgRedux<any>, public devTools: DevToolsExtension) {
 
+    const INITIAL_STATE = {
+      myForm: {
+        name: '',
+        email: '',
+        address: {
+          country: '',
+          city: ''
+        }
+      }
+    };
+
+    this.store.configureStore(
+      defaultFormReducer(),
+      INITIAL_STATE,
+      [reduxLogger.createLogger()],
+      devTools.isEnabled() ? [devTools.enhancer()] : []);
   }
 
+  onFormSubmitted() {
+    console.log(this.store.getState());
+    // Do the work here
+  }
 }
